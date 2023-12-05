64ca2be52ceaca19991b7f463ec00c91fd33d1a8
LeaHergert <lea-hergert@web.de>
2023-12-05 11:45:54 +0100

    edit

---
+++ script.js
@@ -0,0 +1,6 @@
+var explainSpan = document.querySelector("#explain span");
+var explain = document.getElementById("explain");
+var spanHeight = explainSpan.offsetHeight;
+var explainHeight = spanHeight/2 ;
+document.getElementById("explain").style.height = explainHeight + "px";
+
