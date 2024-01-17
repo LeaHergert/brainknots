7601e35dd8e5e3c4a8a617e94c0699da3f69ee01
LeaHergert <lea-hergert@web.de>
2024-01-17 16:25:48 +0100

    styling

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+// Line through "explain your"
+const explainSpan = document.querySelector(".explain span");
+const explain = document.querySelector(".explain");
+const explainSpan2 = document.querySelector(".df span");
+const explain2 = document.querySelector(".df");
+const spanHeight = explainSpan.offsetHeight;
+const spanHeight2 = explainSpan2.offsetHeight;
 
-// Line through "explain your" 
-var explainSpan = document.querySelector(".explain span");
-var explain = document.querySelector(".explain");
-var explainSpan2 = document.querySelector(".df span");
-var explain2 = document.querySelector(".df");
-var spanHeight = explainSpan.offsetHeight;
-var spanHeight2 = explainSpan2.offsetHeight;
-
-var explainHeight = spanHeight/2 ;
-var explainHeight2 = spanHeight2/2 ;
+const explainHeight = spanHeight / 2;
+const explainHeight2 = spanHeight2 / 2;
 document.querySelector(".explain").style.height = explainHeight + "px";
 document.querySelector(".df").style.height = explainHeight2 + "px";
 
+// margin bottom of facts divider
+const hotline = document.querySelector(".hotline");
+const facts = document.getElementById("facts");
 
+const hHeight = hotline.offsetHeight;
+const fHeight = facts.offsetHeight;
 
+facts.style.marginBottom = hHeight + "px";
 
+const mediaQuery = window.matchMedia("(max-width: 600px)");
+
+function drop(nr) {
+  if (mediaQuery.matches) {
+    const content = document.querySelector(".dropdown-content");
+    const drop = document.querySelector("#drop" + nr);
+    const child = document.querySelector("#drop .dropdown-content");
+    const first = drop.firstElementChild;
+    
+    content.style.display = "none";
+    first.style.transform = "rotate(-90)";
+    first.style.transform = "rotate(0)";
+    child.style.display = "grid";
+  }
+}
